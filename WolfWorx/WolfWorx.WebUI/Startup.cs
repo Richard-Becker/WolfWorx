af1ccda9c44494201dd6c077d884e307727b3f06
Richard-Becker <mr.erbecker@gmail.com>
2021-04-16 13:06:22 +0200

    Initial Solution Setup

---
+++ WolfWorx/WolfWorx.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WolfWorx.WebUI.Startup))]
+namespace WolfWorx.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
